cd3e569eb05ce06d61f7bdf55e7e3f2816e0aabe
Hardik Jindal <hardikj22@iitk.ac.in>
2025-04-05 21:57:28 +0530

    template

--- src.c
+++ src.c
@@ -9,6 +9,23 @@ int main(int agc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     double stime1 , stime2 , stime3 , stime4;
     double etime1 , etime2 , etime3 , etime4;
+    int px, py, pz;
+    int nx, ny, nz;
+    int nc;
+    int input_file_size = argv[1]/sizeof(char);
+    int output_file_size = argv[1]/sizeof(char);
+    char *input_file = (char *)malloc(input_file_size);
+    char *output_file = (char *)malloc(output_file_size);
+    
+    input_file = argv[1];
+    px = atoi(argv[2]);
+    py = atoi(argv[3]);
+    pz = atoi(argv[4]);
+    nx = atoi(argv[5]);
+    ny = atoi(argv[6]);
+    nz = atoi(argv[7]);
+    nc = atoi(argv[8]);
+    output_file = argv[9];
     // file read and data distribution
     stime1 = MPI_Wtime();
 
@@ -25,7 +42,7 @@ int main(int agc, char *argv[])
 
     etime3 = MPI_Wtime();
 
-    
+
     // finalize
     stime4 = MPI_Wtime();
 
